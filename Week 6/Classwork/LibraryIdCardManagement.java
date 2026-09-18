2ae54661c3a9f20ff1c8df74e836fd7479bbe00a
AdityaVj26 <av5266@srmist.edu.in>
2026-09-19 01:28:45 +0530

    Added Week 6 Classwork M4 Library ID Card Management

---
+++ Week 6/Classwork/LibraryIdCardManagement.java
@@ -0,0 +1,39 @@
+class IdCard {
+
+    String name;
+    int booksIssued;
+
+    // Constructor
+    public IdCard(String name, int booksIssued) {
+        this.name = name;
+        this.booksIssued = booksIssued;
+    }
+}
+
+public class LibraryIdCardManagement {
+
+    public static void main(String[] args) {
+
+        // Create one IdCard object
+        IdCard ravi = new IdCard("Ravi", 0);
+
+        // Both variables refer to the same object
+        IdCard duplicate = ravi;
+
+        // Change booksIssued using the second variable
+        duplicate.booksIssued = 3;
+
+        // Create a separate object
+        IdCard separate = new IdCard("Ravi", 3);
+
+        // Print results
+        System.out.println(
+                "Ravi's booksIssued (via first variable): "
+                + ravi.booksIssued
+        );
+
+        System.out.println("duplicate == ravi: " + (duplicate == ravi));
+
+        System.out.println("separate == ravi: " + (separate == ravi));
+    }
+}
